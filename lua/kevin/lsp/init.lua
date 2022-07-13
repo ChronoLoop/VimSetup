8530c1fbcf0c268562dd8b11fcf693c277ccfb34
ikevinws <kevinwang1036@gmail.com>
2022-07-13 12:58:14 -0400

    Fix slow saves

--- lua/kevin/lsp/init.lua
+++ lua/kevin/lsp/init.lua
@@ -63,7 +63,7 @@ local function on_attach(client, bufnr)
     )
     -- disable formatting from tsserver
     if client.name == 'tsserver'
-        or client.name == 'jsonls'
+        or client.name == 'jsonls' or client.name == 'html'
     then
         client.resolved_capabilities.document_formatting = false
     end
@@ -71,13 +71,13 @@ local function on_attach(client, bufnr)
     if client.name == 'gopls' then
         vim.opt_local.expandtab = false
     end
-
-    vim.api.nvim_create_autocmd("BufWritePre", {
-        callback = function()
-            vim.lsp.buf.formatting_sync()
-        end,
-    })
 end
+
+vim.api.nvim_create_autocmd("BufWritePre", {
+    callback = function()
+        vim.lsp.buf.formatting_sync()
+    end,
+})
 
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 
