b74b25436fd8698f3bf93e58d893581c74b47c8d
ikevinws <kevinwang1036@gmail.com>
2022-08-29 12:16:38 -0400

    updates

--- lua/kevin/cmp.lua
+++ lua/kevin/cmp.lua
@@ -11,9 +11,9 @@ end
 
 require("luasnip/loaders/from_vscode").lazy_load()
 
-local has_words_before = function()
+local check_backspace = function()
     local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-    return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+    return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match "%s" == nil
 end
 
 -- Setup nvim-cmp
@@ -30,10 +30,15 @@ cmp.setup {
         ['<Tab>'] = cmp.mapping(function(fallback)
             if cmp.visible() then
                 cmp.select_next_item()
+            elseif luasnip.jumpable(1) then
+                luasnip.jump(1)
             elseif luasnip.expand_or_jumpable() then
                 luasnip.expand_or_jump()
-            elseif has_words_before() then
-                cmp.complete()
+            elseif luasnip.expandable() then
+                luasnip.expand()
+            elseif check_backspace() then
+                -- cmp.complete()
+                fallback()
             else
                 fallback()
             end
