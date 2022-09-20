cf0b948658d2fe47105dd114aa769b68a739501e
ikevinws <kevinwang1036@gmail.com>
2022-09-20 17:49:44 -0400

    updates

--- lua/kevin/keybindings.lua
+++ lua/kevin/keybindings.lua
@@ -6,8 +6,8 @@ nnoremap('<C-p>', ':Telescope find_files<CR>')
 
 nnoremap('<C-S><C-S>', ':set invrelativenumber<CR>')
 nnoremap('<C-s>', ':w<CR>')
-nnoremap('<C-s>', ':w<CR>')
 inoremap('<C-s>', '<Esc>:w<CR>a')
+inoremap('<C-c>', '<Esc>')
 
 nnoremap('<C-n>', ':NvimTreeToggle<CR>')
 nnoremap('<leader>r', ':NvimTreeFindFile<CR>')
